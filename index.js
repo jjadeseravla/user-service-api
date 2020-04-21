cbd6d89aaaea4afa2568acd6b85c634e73284a31
jjadeseravla <jadealvares@hotmail.co.uk>
2020-04-21 12:21:23 +0100

    Created an API using Nodejs and Expressjs
    Returning a JSON array on specified port

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3333
 
-app.get('/', (req, res) => res.send('Hello Jade!'))
+app.get('/', (req, res) => res.json([]{
+  name: 'Jade',
+  email: 'ja@gmail.com'
+})])
 
 app.listen(port, () => console.log(`listening on port ${port}!`))
